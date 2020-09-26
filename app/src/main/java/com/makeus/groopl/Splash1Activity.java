c651a611cf4539bdbb87b19f8425da1b6d30253d
jerry <wogkr101@gmail.com>
2020-09-26 16:29:47 +0900

    splash

---
+++ app/src/main/java/com/makeus/groopl/Splash1Activity.java
@@ -0,0 +1,16 @@
+package com.makeus.groopl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.makeus.groopl.src.BaseActivity;
+
+public class Splash1Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash1);
+    }
+}
