71f3c4ac869cc0f97040a236a75496f2fb048e9f
jerry <wogkr101@gmail.com>
2020-09-27 13:16:13 +0900

    group

---
+++ app/src/main/java/com/makeus/groopl/splash3.java
@@ -0,0 +1,14 @@
+package com.makeus.groopl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splash3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash3);
+    }
+}
